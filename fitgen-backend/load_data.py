f1b110db89f753b389b4ad5f6650ef2ee8761434
Ethan Silvas <ethansilvas@gmail.com>
2024-05-13 16:35:28 -0700

    shorten timeframe of workout data

--- fitgen-backend/load_data.py
+++ fitgen-backend/load_data.py
@@ -3,9 +3,11 @@ import numpy as np
 
 workout_df = pd.read_csv(
     './data/WorkoutExport.csv',
-    index_col='Date',
     parse_dates=True,
 )
+
+# select timeframe of workout data 
+workout_df = workout_df[workout_df['Date'] > '2023-06-05']
 
 # convert weight column to lbs 
 kg_to_lbs = 2.20462
@@ -25,7 +27,8 @@ after the previous merges there still may be duplicate exercises for the same da
 workout_df = workout_df.groupby(['Date', 'Exercise', 'Weight', 'Duration(s)', 'isWarmup'])['Reps'].mean().reset_index(name='Reps')
 workout_df['Reps'] = np.ceil(workout_df['Reps'])
 
-
+# remove duplicate exercise rows by using the lowest weight value 
+# workout_df = workout_df.groupby(['Date', 'Exercise', 'Reps', 'Duration(s)', 'isWarmup'])['Weight'].min().reset_index(name='Weight')
 
 workout_df.set_index('Date', inplace=True)
 workout_df.sort_index(ascending=False, inplace=True)
