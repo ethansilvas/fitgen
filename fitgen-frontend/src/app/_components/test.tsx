46c346f4561ab21baccfacbf4f78bef0f57edb44
Ethan Silvas <ethansilvas@gmail.com>
2024-05-14 16:42:14 -0700

    save point

--- fitgen-frontend/src/app/_components/test.tsx
+++ fitgen-frontend/src/app/_components/test.tsx
@@ -1,16 +1,28 @@
 'use client'
 import React, { useEffect, useState } from "react";
 
+
 export default function Test() {
+    // const [isLoading, setLoading] = useState(true)
+    const [workout, setWorkout] = useState({})
+
     useEffect(() => {
         fetch('http://localhost:8080/api/generate').then(
             response => response.json()
         ).then((data) => {
-            console.log(data);
+            setWorkout(data);
+            //setLoading(false);
         });
     }, []);
 
-    return (
-        <div>tester</div>
-    );
+    if (Object.keys(workout).length === 0) {
+        return (
+            <div>Loading...</div>
+        )
+    } else {
+        console.log(workout)
+        return (
+            <div>Data Loaded</div>
+        )
+    }
 }
