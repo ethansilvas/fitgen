0b892f46854f6639852e3a1b02b2d7303a8bc767
Ethan Silvas <ethansilvas@gmail.com>
2024-05-14 11:30:36 -0700

    notes

--- fitgen-backend/api.py
+++ fitgen-backend/api.py
@@ -4,11 +4,39 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+"""
+30 minute
+Shoulders
+Triceps
+(Quadriceps or Hamstrings)
+Biceps
+Chest
+Glutes
+Abs
+Lower Back
+
+
+1 hour
+Back
+Hamstrings
+
+my version
+Shoulders
+Triceps
+(Quadriceps or Hamstrings)
+Biceps
+Chest
+Glutes
+Abs
+(Back or lower back)
+"""
 @app.get('/api/generate')
 def generate_workout():
-    return jsonify({
-        'message': 'hello there'
-    }) 
+    workout = {}
+    
+    
+
+    return jsonify(workout) 
 
 if __name__ == '__main__':
     # default port 5000 will result in CORS issue
