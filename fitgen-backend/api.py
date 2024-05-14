3a85a5fb53887b708eb989d4362bba05e054bcb9
Ethan Silvas <ethansilvas@gmail.com>
2024-05-14 12:15:25 -0700

    first steps of workout gen setup

--- fitgen-backend/api.py
+++ fitgen-backend/api.py
@@ -1,5 +1,9 @@
+from random import randint
+
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+
+from data.exercise_dictionaries import target_muscles
 
 app = Flask(__name__)
 CORS(app)
@@ -30,11 +34,25 @@ Glutes
 Abs
 (Back or lower back)
 """
+
+def choose_legs_back(base_workout):
+    """Uses randint to choose between (quads or hamstrings) and (back or lower back)"""
+    legs = randint(0, 1)
+    back = randint(0, 1)
+
+    base_workout.append('Quadriceps') if legs == 0 else base_workout.append('Hamstrings')
+    base_workout.append('Back') if back == 0 else base_workout.append('Lower Back')
+
 @app.get('/api/generate')
 def generate_workout():
+    base_workout = ['Shoulders', 'Triceps', 'Biceps', 'Chest', 'Glutes', 'Abs']
+
+    choose_legs_back(base_workout)
+
     workout = {}
     
-    
+    for exercise in base_workout:
+        workout[exercise] = []
 
     return jsonify(workout) 
 
